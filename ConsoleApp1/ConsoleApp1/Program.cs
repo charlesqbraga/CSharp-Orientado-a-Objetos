3a9d28be9a9bd0e4ce6c59ad5ced6209cc829f10
Charles Braga <charlesqbraga.dev@gmail.com>
2023-08-02 15:49:41 -0300

    CW artista e nome

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Musica musica1 = new Musica();
+musica1.nome = "Roxane";
+musica1.artista = "The Police";
+musica1.duracao = 1;
+
+Console.WriteLine($"Musica: {musica1.nome}");
+Console.WriteLine($"Artista: {musica1.artista}");
+
+Musica musica2 = new Musica();
+musica2.nome = "Vertogo";
+musica2.nome = "U2";
+
+Console.WriteLine($"\nMusica: {musica2.nome}");
+Console.WriteLine($"Artista: {musica2.artista}");
