a9ba4b5e676bee7a82ca22acecc2252b93ed3f09
Charles Braga <charlesqbraga.dev@gmail.com>
2023-08-02 17:31:41 -0300

    atualizado

--- ConsoleApp1/ConsoleApp1/Musica.cs
+++ ConsoleApp1/ConsoleApp1/Musica.cs
@@ -1,8 +1,21 @@
 ﻿class Musica
 {
-    public string nome;
-    public string artista;
-    public int duracao;
-    public bool disponivel;
+    public string Nome { get; set; }
+    public string Artista { get; set; }
+    public int Duracao { get; set; }
+    public bool Disponivel { get; set; }
+
+    public void ExibirFichaTecnica()
+    {
+        Console.WriteLine($"\nNome: {Nome}");
+        Console.WriteLine($"Artista: {Artista}");
+        Console.WriteLine($"Duração: {Duracao}");
+        if (disponivel)
+        {
+            Console.WriteLine("Disponivel no plano.");
+        } else {
+            Console.WriteLine("Adiquira o plano Plus+.");
+        }
+    }
 
 }
--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,14 +1,18 @@
 ﻿Musica musica1 = new Musica();
-musica1.nome = "Roxane";
-musica1.artista = "The Police";
-musica1.duracao = 1;
+musica1.Nome = "Roxane";
+musica1.Artista = "The Police";
+musica1.Duracao = 273;
+musica1.Disponivel = true;
+Console.WriteLine(musica1.Disponivel);
 
-Console.WriteLine($"Musica: {musica1.nome}");
-Console.WriteLine($"Artista: {musica1.artista}");
+
 
 Musica musica2 = new Musica();
-musica2.nome = "Vertogo";
-musica2.nome = "U2";
+musica2.Nome = "Vertogo";
+musica2.Artista = "U2";
+musica2.Duracao = 367;
+musica2.Disponivel = false;
+Console.WriteLine(musica2.Disponivel);
 
-Console.WriteLine($"\nMusica: {musica2.nome}");
-Console.WriteLine($"Artista: {musica2.artista}");
+musica1.ExibirFichaTecnica();
+musica2.ExibirFichaTecnica();
