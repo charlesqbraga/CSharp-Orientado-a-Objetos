4c8bc5f0debedb95b06df21051b06efbf594cbc2
Charles Braga <charlesqbraga.dev@gmail.com>
2023-08-02 17:55:19 -0300

    atualizado

--- ConsoleApp1/ConsoleApp1/Musica.cs
+++ ConsoleApp1/ConsoleApp1/Musica.cs
@@ -4,13 +4,7 @@
     public string Artista { get; set; }
     public int Duracao { get; set; }
     public bool Disponivel { get; set; }
-    public string DescricaoResumida 
-    {
-        get
-        {
-            return $"A música {Nome} pertence à banda {Artista}.";
-        }
-    }
+    public string DescricaoResumida => $"A música {Nome} pertence à banda {Artista}.";
 
     public void ExibirFichaTecnica()
     {
