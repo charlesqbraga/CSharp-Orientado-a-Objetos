3ee6529a28cc1c3e4540b8da9d14fa97a525697a
Charles Braga <charlesqbraga.dev@gmail.com>
2023-08-04 15:59:03 -0300

    atualizado

---
+++ ConsoleApp1/ConsoleApp1/Album.cs
@@ -0,0 +1,10 @@
+﻿class Album
+{
+    private List<Musica> musicas = new List<Musica>();
+    public string Nome { get; set; }
+    public int DuracaoTotal { get; set; }
+    public void AdicionarMusica(Musica musica)
+    {
+        musicas.Add(musica);
+    }
+}
--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,18 +1,11 @@
-﻿Musica musica1 = new Musica();
-musica1.Nome = "Roxane";
-musica1.Artista = "The Police";
-musica1.Duracao = 273;
-musica1.Disponivel = true;
-Console.WriteLine(musica1.DescricaoResumida);
+﻿Album albumDoQueen = new Album();
+albumDoQueen.Nome = "A night at the opera";
 
-
+Musica musica1 = new Musica();
+musica1.Nome = "Love of my life";
 
 Musica musica2 = new Musica();
-musica2.Nome = "Vertogo";
-musica2.Artista = "U2";
-musica2.Duracao = 367;
-musica2.Disponivel = false;
-Console.WriteLine(musica2.DescricaoResumida);
+musica2.Nome = "Bohemia Rhapsody";
 
-musica1.ExibirFichaTecnica();
-musica2.ExibirFichaTecnica();
+albumDoQueen.AdicionarMusica(musica1);
+albumDoQueen.AdicionarMusica(musica2);
