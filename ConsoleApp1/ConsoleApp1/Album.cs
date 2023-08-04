0ae1a8cd41f1288115d4edfb6b785258c092a7f0
Charles Braga <charlesqbraga.dev@gmail.com>
2023-08-04 16:38:02 -0300

    implementada duração do álbum

--- ConsoleApp1/ConsoleApp1/Album.cs
+++ ConsoleApp1/ConsoleApp1/Album.cs
@@ -2,9 +2,19 @@
 {
     private List<Musica> musicas = new List<Musica>();
     public string Nome { get; set; }
-    public int DuracaoTotal { get; set; }
+    public int DuracaoTotal => musicas.Sum(m => m.Duracao);
     public void AdicionarMusica(Musica musica)
     {
         musicas.Add(musica);
+    }
+    public void ExibirMusicasDoAlbum()
+    {
+        Console.WriteLine($"Lista de músicas do álbum {Nome}:\n");
+        foreach (var musica in musicas)
+        {
+            Console.WriteLine($"Musica: {musica.Nome}");
+
+        }
+        Console.WriteLine($"Para ouvir este álbum inteiro você precisa de {DuracaoTotal} segundos.");
     }
 }
