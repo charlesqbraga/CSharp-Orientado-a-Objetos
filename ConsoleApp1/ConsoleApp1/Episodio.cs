12a659309efa13dcffec2beb63efd8d778f7cc2e
Charles Braga <charlesqbraga.dev@gmail.com>
2023-08-07 20:50:19 -0300

    atualizado

---
+++ ConsoleApp1/ConsoleApp1/Episodio.cs
@@ -0,0 +1,20 @@
+﻿class Episodio
+{
+    private List<string> convidados = new();
+    public Episodio(int ordem, int titulo, int duracao)
+    {
+        Ordem = ordem;
+        Titulo = titulo;
+        Duracao = duracao;
+    }
+
+    public int Ordem { get; }
+    public int Titulo { get; }
+    public int Duracao { get; }
+    public string Resumo => $"{Ordem}. {Titulo} ({Duracao} min) - {string.Join(", " , convidados)}";
+
+    public void AdicionarConvidados(string convidado)
+    {
+        convidados.Add(convidado);
+    }
+}
